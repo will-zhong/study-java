687aaef90a347c77ce0a627ef07952b7737ef84c
zhiwei.zhong <zuiwei0@gmail.com>
2020-11-02 19:19:34 +0800

    新增算法

---
+++ algorithm/algorithm-bloomfilter/src/main/java/com/wei/study/java/algorithm/bloomfilter/HashFunction.java
@@ -0,0 +1,22 @@
+package com.wei.study.java.algorithm.bloomfilter;
+
+public class HashFunction {
+
+    private int size;
+    private int seed;
+
+    public HashFunction(int size, int seed) {
+        this.size = size;
+        this.seed = seed;
+    }
+
+    public int hash(String value) {
+        int result = 0;
+        int len = value.length();
+        for (int i = 0; i < len; i++) {
+            result = seed * result + value.charAt(i);
+        }
+        int r = (size - 1) & result;
+        return (size - 1) & result;
+    }
+}
