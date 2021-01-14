21b295be539022a901c80e5e11246ea858200533
zhongzhiwei <zhongzhiwei@corp.netease.com>
2021-01-14 21:10:56 +0800

    Add apollo project

---
+++ apollo/apollo-app/src/main/java/com/wei/study/java/apollo/AppConfig.java
@@ -0,0 +1,7 @@
+package com.wei.study.java.apollo;
+
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+
+
+public class AppConfig {
+}
---
+++ apollo/apollo-app/src/main/java/com/wei/study/java/apollo/Application.java
@@ -0,0 +1,32 @@
+package com.wei.study.java.apollo;
+
+import com.ctrip.framework.apollo.spring.annotation.ApolloConfig;
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@EnableApolloConfig
+@SpringBootApplication
+@RestController
+public class Application {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+    }
+
+
+    @Value("${apollo.test.value:tttt}")
+    private String test;
+
+    @RequestMapping("index")
+    public Object index() {
+        return test;
+    }
+
+}
